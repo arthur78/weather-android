52dd6f0cbcd8f0ff409845f24fa909752b7cd0af
Arthur Khakimov <arthur78@cloud.upwork.com>
2017-05-06 03:05:57 +0300

    Add Houry list view (RecyclerView)

---
+++ app/src/main/java/com/artxak/stormy/adapters/HourAdapter.java
@@ -0,0 +1,61 @@
+package com.artxak.stormy.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.artxak.stormy.R;
+import com.artxak.stormy.weather.Hour;
+
+public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
+
+    private Hour[] mHours;
+
+    public HourAdapter(Hour[] hours) {
+        mHours = hours;
+    }
+
+    @Override
+    public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.hourly_list_item, parent, false);
+        return new HourViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(HourViewHolder holder, int position) {
+        holder.bindHour(mHours[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHours.length;
+    }
+
+    public class HourViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTimeLabel;
+        public TextView mTemperatureLabel;
+        public TextView mSummaryLabel;
+        public ImageView mIconImageView;
+
+        public HourViewHolder(View itemView) {
+            super(itemView);
+
+            mTimeLabel = (TextView) itemView.findViewById(R.id.timeLabel);
+            mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
+            mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
+            mIconImageView = (ImageView) itemView.findViewById(R.id.iconImageView);
+        }
+
+        public void bindHour(Hour hour) {
+            mTimeLabel.setText(hour.getHour());
+            mSummaryLabel.setText(hour.getSummary());
+            mTemperatureLabel.setText(hour.getTemperature() + "°");
+            mIconImageView.setImageResource(hour.getIconId());
+        }
+    }
+}
--- app/src/main/java/com/artxak/stormy/weather/Hour.java
+++ app/src/main/java/com/artxak/stormy/weather/Hour.java
@@ -1,11 +1,46 @@
 package com.artxak.stormy.weather;
 
-public class Hour {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Hour implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperature;
     private String mIcon;
     private String mTimeZone;
+
+    public Hour() {}
+
+    protected Hour(Parcel in) {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperature = in.readDouble();
+        mIcon = in.readString();
+        mTimeZone = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel in) {
+            return new Hour(in);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
+
+    public String getHour() {
+        SimpleDateFormat formatter = new SimpleDateFormat("H", Locale.US);
+        Date dateTime = new Date(mTime * 1000);
+        return formatter.format(dateTime);
+    }
 
     public long getTime() {
         return mTime;
@@ -23,8 +58,9 @@ public class Hour {
         mSummary = summary;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        double celsiusTemperature = (mTemperature - 32) / 1.8;
+        return (int) Math.round(celsiusTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -39,11 +75,29 @@ public class Hour {
         mIcon = icon;
     }
 
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
+    }
+
     public String getTimeZone() {
         return mTimeZone;
     }
 
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mIcon);
+        dest.writeString(mTimeZone);
     }
 }
